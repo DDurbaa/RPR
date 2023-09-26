1e76904d73d9913aeff608d79001f64828685c5d
Durbaa <143736812+DDurbaa@users.noreply.github.com>
2023-09-26 09:13:27 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zvireprorytire
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string vstup = textBox1.Text.Trim();
+            int pocet = 0;
+            foreach(char c in vstup)
+            {
+                if(c ==' ')
+                {
+                    pocet++;
+                }    
+            }
+            MessageBox.Show("pocet slov " + pocet);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+             
+        }
+    }
+}
